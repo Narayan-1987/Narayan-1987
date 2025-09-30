43e8c4204f5284ff4245f64910de8fd75ddcef8b
Narayan-1987 <narayanpachaury567@gmail.com>
2025-09-30 21:59:25 +0530

    first commit

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/page.jsx";
 import About from "../pages/about/page.jsx";
-import Services from "../pages/services/Page.jsx";
+import Services from "../pages/services/page.jsx";
 import Project from "../pages/project/page.jsx";
 import Blogs from "../pages/blog/page.jsx";
 import Contact from "../pages/contact/page.jsx";
